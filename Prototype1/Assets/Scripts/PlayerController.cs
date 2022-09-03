613ab77ae7461229bc4cb614fd512e3d2d146cd1
jlbeck <jlbeck@mail.bradley.edu>
2022-09-03 12:23:36 -0500

    Added Prototype 1 Game

---
+++ Prototype1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed;
+    public float turnSpeed;
+
+    public float horizontalInput;
+    public float forwardInput;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        forwardInput = Input.GetAxis("Vertical");
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        //Move forward with forward input
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+
+        //Rotate player with horizontal input
+        transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime * horizontalInput);
+
+    }
+}
---
+++ Prototype1/Assets/Scripts/PlayerEnterTrigger.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//attach this to player
+public class PlayerEnterTrigger : MonoBehaviour
+{
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("TriggerZone"))
+        {
+            ScoreManager.score++;
+        }
+    }
+
+}
