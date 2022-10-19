9127d68034b8868131c5d60fedf28e7c761c2898
jlbeck <jlbeck@mail.bradley.edu>
2022-10-19 18:46:38 -0500

    Followed along up to 1:30:47

---
+++ Prototype 5B/Assets/MyFirstPersonPlayer/Scripts/MouseLook.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+
+    public float mouseSensitivity = 100f;
+    public GameObject player;
+    private float verticalLookRotation = 0f;
+
+    void Update()
+    {
+        //Get mouse input and assign it to two floats
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        //Rotate player GameObject with horizontal mouse input
+        player.transform.Rotate(Vector3.up * mouseX);
+
+        //Rotate camera around the X axis with vertical mouse input
+        verticalLookRotation -= mouseY;
+
+        //Limit vertical rotation with clamp
+        verticalLookRotation = Mathf.Clamp(verticalLookRotation, -90f, 90f);
+        //Apply rotation to our camera based on clamped output
+        transform.localRotation = Quaternion.Euler(verticalLookRotation, 0f, 0f);
+
+    }
+
+    private void OnApplicationFocus(bool focus)
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+}
---
+++ Prototype 5B/Assets/MyFirstPersonPlayer/Scripts/PlayerMovement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public CharacterController controller;
+    public float speed = 12f;
+    public Vector3 velocity;
+    public float gravity = -9.81f;
+    public float gravityMultiplier = 2f;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+    public bool isGrounded;
+
+    private void Awake()
+    {
+        gravity *= gravityMultiplier;
+    }
+
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+        controller.Move(move * speed * Time.deltaTime);
+
+        //add gravity to velocity
+        velocity.y += gravity * Time.deltaTime;
+        //we multiply gravity by Time.deltaTime again to simulate gravity accelerating in a free fall
+        controller.Move(velocity * Time.deltaTime);
+
+        //Check if the player is on the ground
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+    }
+}
