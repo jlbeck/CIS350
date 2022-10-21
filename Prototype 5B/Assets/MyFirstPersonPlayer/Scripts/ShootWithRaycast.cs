bb4d3b83f2514feac5874179ebf58819e9700e03
jlbeck <jlbeck@mail.bradley.edu>
2022-10-21 12:06:20 -0500

    Finished follow along video

--- Prototype 5B/Assets/MyFirstPersonPlayer/Scripts/ShootWithRaycast.cs
+++ Prototype 5B/Assets/MyFirstPersonPlayer/Scripts/ShootWithRaycast.cs
@@ -8,6 +8,10 @@ public class ShootWithRaycast : MonoBehaviour
     public float range = 100f;
     public Camera cam;
 
+    public ParticleSystem muzzleFlash;
+
+    public float hitForce = 10f;
+
     private void Update()
     {
         if (Input.GetButtonDown("Fire1"))
@@ -19,12 +23,29 @@ public class ShootWithRaycast : MonoBehaviour
 
     void Shoot()
     {
+
+        muzzleFlash.Play();
+
         RaycastHit hitInfo;
 
         //triggers if something is hit with ray
         if (Physics.Raycast(cam.transform.position, cam.transform.forward, out hitInfo, range))
         {
             Debug.Log(hitInfo.transform.gameObject.name);
+
+            Target target = hitInfo.transform.gameObject.GetComponent<Target>();
+
+            if (target != null)
+            {
+                target.TakeDamage(damage);
+            }
+
+            //If shot hits a RigidBody, apply a force
+            if (hitInfo.rigidbody != null)
+            {
+                hitInfo.rigidbody.AddForce(cam.transform.TransformDirection(Vector3.forward) * hitForce, ForceMode.Impulse);
+            }
+
         }
 
     }
