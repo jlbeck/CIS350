0c4c8ea0beb5342dbfcc454817aab5d290ffd197
jlbeck <jlbeck@mail.bradley.edu>
2022-11-22 11:04:15 -0600

    Prototype 5 up to 22:39

---
+++ Prototype5/Assets/Scripts/Target.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+
+    private Rigidbody targetRb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetRb = GetComponent<Rigidbody>();
+
+        //add upwards force multiplied by a randomized speed
+        targetRb.AddForce(Vector3.up * Random.Range(12, 16), ForceMode.Impulse);
+
+        //add a torque with randomized xyz values
+        targetRb.AddTorque(Random.Range(-10, 10), Random.Range(-10, 10), Random.Range(-10, 10), ForceMode.Impulse);
+
+        //set the position with a randomized X value
+        transform.position = new Vector3(Random.Range(-4, 4), -6);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
